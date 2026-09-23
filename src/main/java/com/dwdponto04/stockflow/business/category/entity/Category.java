2816c6d675c5c57480e05343c4c7daa751e7f945
David <davidfl44.pro@gmail.com>
2026-09-23 00:04:04 -0300

    feat: implement Category entity

---
+++ src/main/java/com/dwdponto04/stockflow/business/category/entity/Category.java
@@ -0,0 +1,29 @@
+package com.dwdponto04.stockflow.business.category.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Setter
+    @Column(name = "name",length =50, unique = true, nullable = false)
+    private String name;
+}
